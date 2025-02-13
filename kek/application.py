426d198fd698383feba5575123362d0b1f41bc00
Ghostkeeper <rubend@tutanota.com>
2025-02-13 21:22:12 +0100

    Temp

--- kek/application.py
+++ kek/application.py
@@ -1,5 +1,5 @@
 # Desktop environment for a domotics hub.
-# Copyright (C) 2024 Ghostkeeper
+# Copyright (C) 2025 Ghostkeeper
 # This application is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 # This application is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for details.
 # You should have received a copy of the GNU Affero General Public License along with this application. If not, see <https://gnu.org/licenses/>.
@@ -21,6 +21,9 @@ import kek.video_directory
 import kek.video_player
 
 
+instance = None
+
+
 class Application(PySide6.QtWidgets.QApplication):
 	"""
 	The Qt application that runs the whole thing.
@@ -38,6 +41,8 @@ class Application(PySide6.QtWidgets.QApplication):
 		Begins the start-up process.
 		:param argv: Command-line parameters provided to the application. Qt understands some of these.
 		"""
+		global instance
+		instance = self
 		logging.info(f"Starting application version {Application.version}.")
 		super().__init__(argv)
 
@@ -51,13 +56,15 @@ class Application(PySide6.QtWidgets.QApplication):
 		PySide6.QtQml.qmlRegisterSingletonInstance(kek.map.Map, "Kek", 1, 0, "Map", kek.map.Map.get_instance())
 		PySide6.QtQml.qmlRegisterSingletonInstance(kek.music_player.MusicPlayer, "Kek", 1, 0, "MusicPlayer", kek.music_player.MusicPlayer.get_instance())
 		PySide6.QtQml.qmlRegisterSingletonInstance(kek.playlist.Playlist, "Kek", 1, 0, "Playlist", kek.playlist.Playlist.get_instance())
-		PySide6.QtQml.qmlRegisterSingletonInstance(kek.video_player.VideoPlayer, "Kek", 1, 0, "VideoPlayer", kek.video_player.VideoPlayer.get_instance())
 		PySide6.QtQml.qmlRegisterType(kek.music_directory.MusicDirectory, "Kek", 1, 0, "MusicDirectory")
+		PySide6.QtQml.qmlRegisterSingletonInstance(kek.video_player.VideoPlayer, "Kek", 1, 0, "VideoPlayer", kek.video_player.VideoPlayer.get_instance())
 		PySide6.QtQml.qmlRegisterType(kek.video_directory.VideoDirectory, "Kek", 1, 0, "VideoDirectory")
+
 
 		logging.debug("Loading QML engine.")
 		self.engine = PySide6.QtQml.QQmlApplicationEngine()
 		self.engine.quit.connect(self.quit)
+		logging.debug("Creating main window.")
 		self.engine.load("gui/MainWindow.qml")
 
 		logging.info("Start-up complete.")
