976ac163564925548fbd8162e65baf0ea064ec5c
Ghostkeeper <rubend@tutanota.com>
2025-02-22 22:47:52 +0100

    Fix films directory

--- kek/video_directory.py
+++ kek/video_directory.py
@@ -46,7 +46,7 @@ class VideoDirectory(PySide6.QtCore.QAbstractListModel):
 		self.videos: list[dict[str, typing.Any]] = []  # The actual data contained in this table.
 		self._sort_by = "rating"
 
-		self.base_directory = "/films"
+		self.base_directory = "/films/"
 		self._default_directory = "Films"
 		self._directory = ""
 		self.directory_set(self.base_directory + self._default_directory)
